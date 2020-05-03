1c7c1229800db4ee137b7f42f3f7cce2bcf12b37
Дмитрий Воронецкий <vda1999@yandex.ru>
2020-05-03 17:33:14 +0300

    3squares split into different functions

--- 3squares.py
+++ 3squares.py
@@ -2,6 +2,32 @@ import oracle
 import numpy as np
 
 
+def psi_cup(x0, L, addend, oracle_):
+    quotient = 1 / (2 * np.linalg.norm(oracle_.func(x0)))
+    loss = (L / 2) * np.linalg.norm(addend) ** 2
+    loss += quotient * np.linalg.norm(oracle_.func(x0)) ** 2
+    loss += quotient * np.linalg.norm(oracle_.func(x0) + oracle_.grad(x0) * addend) ** 2
+    return loss
+
+
+def optimize_psi_cup(x0, i, oracle_, magic_const):
+    best_y = None
+    best_y_loss = None
+    L = 1
+    for j in range(magic_const):
+        addend = np.random.rand(*x0.shape)
+        addend /= np.linalg.norm(addend)
+        addend *= np.linalg.norm(x0)
+        addend /= (i ** 1.5 + 15)
+
+        loss = psi_cup(x0, L, addend, oracle_)
+
+        if best_y_loss is None or best_y_loss > loss:
+            best_y = x0 + addend
+            best_y_loss = loss
+    return best_y, best_y_loss
+
+
 # here func is F, not f=tf.norm(F)
 def do_3squares(func, dimension, x0=None, max_iter=1000, tolerance=2e-3, eta=5e-1, magic_const=100):
     losses = []
@@ -10,24 +36,7 @@ def do_3squares(func, dimension, x0=None, max_iter=1000, tolerance=2e-3, eta=5e-
     oracle_ = oracle.BaseSmoothOracle(func)
 
     for i in range(max_iter):
-        best_y = None
-        best_y_loss = None
-        L = 1
-        for j in range(magic_const):
-            addend = np.random.rand(*x0.shape)
-            addend /= np.linalg.norm(addend)
-            addend *= np.linalg.norm(x0)
-            addend /= (i ** 1.5 + 15)
-
-            quotient = 1 / (2 * np.linalg.norm(oracle_.func(x0)))
-
-            loss = (L / 2) * np.linalg.norm(addend) ** 2
-            loss += quotient * np.linalg.norm(oracle_.func(x0)) ** 2
-            loss += quotient * np.linalg.norm(oracle_.func(x0) + oracle_.grad(x0) * addend) ** 2
-
-            if best_y_loss is None or best_y_loss > loss:
-                best_y = x0 + addend
-                best_y_loss = loss
+        best_y, best_y_loss = optimize_psi_cup(x0, i, oracle_, magic_const)
 
         if np.linalg.norm(best_y - x0) < tolerance:
             break
