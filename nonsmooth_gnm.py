fc58403e03a7d8696241dc2fa439e74c4b7f0da7
ilya-pauzner <ilya-pauzner@yandex.ru>
2020-05-03 16:02:56 +0300

    non-smooth gnm (1.16) in paper

---
+++ nonsmooth_gnm.py
@@ -0,0 +1,43 @@
+import numpy as np
+
+import oracle
+
+
+# here func is F, not f=tf.norm(F)
+def do_nonsmooth_gnm(func, dimension, x0=None, max_iter=1000, tolerance=2e-3, eta=5e-1):
+    losses = []
+    if x0 is None:
+        x0 = np.array([0.57179] * dimension, dtype=np.float64)
+    oracle_ = oracle.BaseSmoothOracle(func)
+
+    for i in range(max_iter):
+        best_y = None
+        best_y_loss = None
+        L = 1
+        for j in range(100):
+            addend = np.random.rand(*x0.shape)
+            addend /= np.linalg.norm(addend)
+            addend *= np.linalg.norm(x0)
+            addend /= (i ** 1.5 + 15)
+
+            loss = (L / 2) * np.linalg.norm(addend) ** 2
+            loss += np.linalg.norm(oracle_.func(x0) + oracle_.grad(x0) * addend)
+
+            if best_y_loss is None or best_y_loss > loss:
+                best_y = x0 + addend
+                best_y_loss = loss
+
+        if np.linalg.norm(best_y - x0) < tolerance:
+            break
+        x0 = best_y
+
+        loss = np.linalg.norm(oracle_.func(x0))
+        print("point:", x0, "loss:", loss, "iter:", i)
+        losses.append(loss)
+
+    return x0
+
+
+if __name__ == '__main__':  # testing
+    print(do_nonsmooth_gnm(lambda x: oracle.apply_func_F(x, oracle.trigonometry), 2, eta=1e-2))
+    print(do_nonsmooth_gnm(lambda x: oracle.apply_func_F(x, oracle.chebyshev), 2, eta=1e-2))
